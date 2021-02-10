cf84479b046a317dc49e837dadae3b170fc0cfdd
Trevor Smith <trevor.smith1667@gmail.com>
2021-02-10 15:00:07 -0800

    refactoring session and demo

--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -14,7 +14,7 @@ class SessionForm extends React.Component {
     }
 
     
-    componentWillUnmount() {
+    componentDidMount() {
         this.props.clearErrors();
     }
     
@@ -55,53 +55,37 @@ class SessionForm extends React.Component {
     }
 
     render() {
-        let demoButton = (<></>)
         if (this.props.formType === 'login') {
-            demoButton = <a onClick={this.loginDemo}>Demo Login</a>
-                
-        }
-       
-        return (
-            <div className="login-form-container">
-                <form onSubmit={this.handleSubmit} className="login-form-box">
-                    <h1 className="modal-header">Please Sign In</h1>
-                    <div onClick={this.props.closeModal} className="close-x">X</div>
-                    {this.renderErrors()}
 
-                    <div className="login-form">
-                        <input type="string"
-                            value={this.state.username}
-                            onChange={this.update('username')}
-                            className="login-input"
-                            placeholder="Username"/>
+            return (
+                <div className="login-form-container">
+                    <form onSubmit={this.handleSubmit} className="login-form-box">
+                        <h1 className="modal-header">Please Sign In</h1>
+                        {this.renderErrors()}
+
+                        <div className="login-form">
+                            <input type="string"
+                                value={this.state.username}
+                                onChange={this.update('username')}
+                                className="login-input"
+                                placeholder="Username"/>
                             
 
-                        <input type= "string"
-                            value={this.state.email}
-                            onChange={this.update('email')}
-                            className="login-input"
-                            placeholder="Email"/>
+                            <input type= "string"
+                                value={this.state.email}
+                                onChange={this.update('email')}
+                                className="login-input"
+                                placeholder="Email"/>
                         
                     
                         <input type="password"
-                            value={this.state.password}
-                            onChange={this.update('password')}
-                            className="login-input"
-                            placeholder="Password"/>
+                                value={this.state.password}
+                                onChange={this.update('password')}
+                                className="login-input"
+                                placeholder="Password"/>
                        
                     
-                        <type="submit" className="login_button">Sign In</button>
-                    </div>
-
-
-
-                    
-                    <div className="modal-footer">
-                        
-                                <a 
-                                className='signup-link'
-                                onClick={ () =>this.props.openModal('signup') }>Create an account
-                                </a>
+                        <button type="submit" className="login_button">Sign In</button>
                     </div>
                 </form>
             </div>
