e9b4fd39bff794884c29d4e02097cea270129753
Trevor Smith <trevor.smith1667@gmail.com>
2021-02-13 17:44:49 -0800

    removed space in yml

--- frontend/components/nav/nav.jsx
+++ frontend/components/nav/nav.jsx
@@ -5,6 +5,7 @@ import GreetingContainer from '../greeting/greeting_container';
 const Nav = () => {
     return (
         <header className="nav">
+            
             <Link to="/" className="nav-header">
                 <img id="nav-header-img" src={window.logoURL} />
                 <h1 id="app-title">ForkFull</h1>
