1524d70a649ac7075e7529d1381a927ea8f5c7b0
Trevor Smith <trevor.smith1667@gmail.com>
2021-02-17 12:20:15 -0800

    changed background image

--- frontend/components/restaurant/restaurant_index.jsx
+++ frontend/components/restaurant/restaurant_index.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from  'react-router-dom'
  
 class RestaurantIndex extends React.Component {
     constructor(props) {
@@ -22,6 +23,20 @@ class RestaurantIndex extends React.Component {
     render(){
         let restArr = randomizeRest(this.props.restaurants.slice(0))
         restArr.slice(0, 14)
-        
+
+        return (
+        <div>
+            {restArr.map(rest => (
+            <Link to={`/restaurants/${restaurant.id}`} key={restaurant.id}>
+                <div className='rest-unit'>
+                    <img src={rest.photos[0]} className="unit-photo"/>
+                   <div className='unit-info'>
+                   </div>
+                </div>
+            </Link>
+            ))}
+        </div>
+
+        ) 
     }
 }
